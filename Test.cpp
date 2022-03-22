070f191b47fc5eb32653a63453777c647c2eab39
TalMalchi <malchital@gmail.com>
2022-03-22 14:48:04 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -11,16 +11,16 @@ TEST_CASE("write"){
 
 Notebook notebook_test;
 
-CHECK_THROWS(notebook_test.write(2,3,6,Direction::Horizontal, "hiii"));
-CHECK_THROWS(notebook_test.write(4,5,5,Direction::Horizontal, "aa"));
-CHECK_THROWS(notebook_test.write(7,7,6,Direction::Horizontal, "home"));
-CHECK_THROWS(notebook_test.write(2,5,20,Direction::Horizontal, "hiii"));
-CHECK_THROWS(notebook_test.write(9,5,3,Direction::Horizontal, "fun"));
-CHECK_THROWS(notebook_test.write(13,7,9,Direction::Vertical, "sky"));
-CHECK_THROWS(notebook_test.write(6,8,6,Direction::Vertical, "aaaa"));
-CHECK_THROWS(notebook_test.write(2,9,10,Direction::Vertical, "xyz"));
-CHECK_THROWS(notebook_test.write(2,5,3,Direction::Vertical, "hiii"));
-CHECK_NOTHROW(notebook_test.write(1,0,3,Direction::Vertical, ""));
+CHECK_NOTHROW(notebook_test.write(2,3,6,Direction::Horizontal, "hiii"));
+CHECK_NOTHROW(notebook_test.write(4,5,5,Direction::Horizontal, "aa"));
+CHECK_NOTHROW(notebook_test.write(7,7,6,Direction::Horizontal, "home"));
+CHECK_NOTHROW(notebook_test.write(2,5,20,Direction::Horizontal, "hiii"));
+CHECK_NOTHROW(notebook_test.write(9,5,3,Direction::Horizontal, "fun"));
+CHECK_NOTHROW(notebook_test.write(13,7,9,Direction::Vertical, "sky"));
+CHECK_NOTHROW(notebook_test.write(6,8,6,Direction::Vertical, "aaaa"));
+CHECK_NOTHROW(notebook_test.write(2,9,10,Direction::Vertical, "xyz"));
+CHECK_NOTHROW(notebook_test.write(2,5,3,Direction::Vertical, "hiii"));
+CHECK_THROWS(notebook_test.write(1,0,3,Direction::Vertical, ""));
 
 }
 
@@ -41,10 +41,10 @@ TEST_CASE("erase"){
 Notebook notebook_test;
 //read or write string and then erase it 
 CHECK(notebook_test.read(2,5,20,Direction::Horizontal,6)=="tal___");
-CHECK_THROWS(notebook_test.erase(2,5,20,Direction::Horizontal, 5));
-CHECK_THROWS(notebook_test.write(2,5,3,Direction::Vertical, "hiii"));
-CHECK_THROWS(notebook_test.erase(2,5,3,Direction::Horizontal, 7));
-CHECK_THROWS(notebook_test.erase(7,7,6,Direction::Horizontal, 4));
-CHECK_NOTHROW(notebook_test.erase(2,1,3,Direction::Horizontal, -1));
-CHECK_NOTHROW(notebook_test.erase(9,5,3,Direction::Horizontal, 0));
+CHECK_NOTHROW(notebook_test.erase(2,5,20,Direction::Horizontal, 5));
+CHECK_NOTHROW(notebook_test.write(2,5,3,Direction::Vertical, "hiii"));
+CHECK_NOTHROW(notebook_test.erase(2,5,3,Direction::Horizontal, 7));
+CHECK_NOTHROW(notebook_test.erase(7,7,6,Direction::Horizontal, 4));
+CHECK_THROWS(notebook_test.erase(2,1,3,Direction::Horizontal, -1));
+CHECK_THROWS(notebook_test.erase(9,5,3,Direction::Horizontal, 0));
 }
