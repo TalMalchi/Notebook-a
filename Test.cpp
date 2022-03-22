05bc82ff132b09e2e54e8bd3f2709d5846ced016
TalMalchi <malchital@gmail.com>
2022-03-22 14:59:20 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -20,7 +20,7 @@ CHECK_NOTHROW(notebook_test.write(13,7,9,Direction::Vertical, "sky"));
 CHECK_NOTHROW(notebook_test.write(6,8,6,Direction::Vertical, "aaaa"));
 CHECK_NOTHROW(notebook_test.write(2,9,10,Direction::Vertical, "xyz"));
 CHECK_NOTHROW(notebook_test.write(2,5,3,Direction::Vertical, "hiii"));
-CHECK_THROWS(notebook_test.write(1,0,3,Direction::Vertical, ""));
+CHECK_THROWS(notebook_test.write(1,0,95,Direction::Vertical, "aaaaaa"));
 
 }
 
@@ -33,6 +33,8 @@ CHECK(notebook_test.read(2,3,6,Direction::Horizontal, 2)=="hii");
 CHECK(notebook_test.read(4,5,5,Direction::Horizontal, 2)=="aa");
 CHECK(notebook_test.read(7,7,6,Direction::Horizontal, 4)== "home");
 CHECK(notebook_test.read(13,7,9,Direction::Horizontal,5)=="sky__");
+CHECK_THROWS(notebook_test.read(1,0,95,Direction::Vertical, 101));
+
 
 }
 
@@ -45,6 +47,6 @@ CHECK_NOTHROW(notebook_test.erase(2,5,20,Direction::Horizontal, 5));
 CHECK_NOTHROW(notebook_test.write(2,5,3,Direction::Vertical, "hiii"));
 CHECK_NOTHROW(notebook_test.erase(2,5,3,Direction::Horizontal, 7));
 CHECK_NOTHROW(notebook_test.erase(7,7,6,Direction::Horizontal, 4));
-CHECK_THROWS(notebook_test.erase(2,1,3,Direction::Horizontal, -1));
-CHECK_THROWS(notebook_test.erase(9,5,3,Direction::Horizontal, 0));
+CHECK_THROWS(notebook_test.erase(2,1,3,Direction::Horizontal, 104));
+CHECK_THROWS(notebook_test.erase(9,5,98,Direction::Horizontal, 5));
 }
